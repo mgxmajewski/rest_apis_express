03ad411ec45caf1a1e9e3f79c34a7a980f15ce06
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-21 23:12:49 +0100

    Add handler for /quotes/:id

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get('/greetings', (req, res)=>{
-    res.json({greeting: "Hello, World!"})
-});
-/*
-TODO:
 // Send a GET request to /quotes READ a list of quotes
+app.get('/quotes', (req, res)=>{
+    res.json(data);
+});
+
 // Send a GET request to /quotes/:id to READ(view) a quote
-// Send a POST request to /quotes to CREATE a new quote
-// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
-// Send a DELETE request to /quotes/:id to DELETE a quote
-// Send a GET request to /quotes/quote/random to READ (view) a random quote
-* */
+app.get('/quotes/:id', (req, res)=>{
+    const quote = data.quotes.find(quote => quote.id == req.params.id);
+    res.json(quote);
+});
+
+//TODO:
+//    - Send a GET request to /quotes READ a list of quotes
+//    - Send a GET request to /quotes/:id to READ(view) a quote
+//    - Send a POST request to /quotes to CREATE a new quote
+//    - Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+//    - Send a DELETE request to /quotes/:id to DELETE a quote
+//    - Send a GET request to /quotes/quote/random to READ (view) a random quote
 
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
