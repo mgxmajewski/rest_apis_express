2df02919e613a85d2f982c6c5734e3d6d4801bcf
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-24 02:12:21 +0100

    Add asyncHandler() to reduce verbosity of try catch blocks

--- app.js
+++ app.js
@@ -3,54 +3,50 @@ const app = express();
 
 const records = require('./records');
 
+function asyncHandler(cb){
+    return async (req, res, next) => {
+        try {
+            await cb(req, res,next);
+        } catch (err) {
+            next(err);
+        }
+    }
+}
+
 app.use(express.json());
 
 // Send a GET request to /quotes READ a list of quotes
-app.get('/quotes', async (req, res)=>{
-    try{
+app.get('/quotes', asyncHandler(async (req, res)=>{
         const quotes = await records.getQuotes();
         res.json(quotes);
-    }catch (err) {
-        res.status(500).json({message: err.message});
-    }
-});
+}));
 
 // Send a GET request to /quotes/:id to READ(view) a quote
-app.get('/quotes/:id', async (req, res)=>{
-    try{
+app.get('/quotes/:id', asyncHandler(async (req, res)=>{
         const quote = await records.getQuote(req.params.id);
         if(quote){
             res.json(quote);
         } else {
             res.status(404).json({message: "Not found dude"});
         }
-
-    }catch (err) {
-        res.status(500).json({message: err.message});
-    }
-});
+}));
 
 //    - Send a POST request to /quotes to CREATE a new quote
-app.post('/quotes', async(req, res) =>{
-    try{
-        if(req.body.author && req.body.quote){
-            const quote = await records.createQuote({
-                quote: req.body.quote,
-                author: req.body.author
-            });
-            res.status(201).json(quote);
-        } else{
-            res.status(400).json({message: "Author and quote required."});
-        }
 
-    }catch (err){
-        res.status(500).json({message: err.message});
+app.post('/quotes', asyncHandler(async (req, res ) => {
+    if(req.body.author && req.body.quote){
+        const quote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });
+        res.status(201).json(quote);
+    } else{
+        res.status(400).json({message: "Author and quote required."});
     }
-});
+}));
 
 //    - Send a PUT request to /quotes/:id to UPDATE (edit) a quote
-app.put('/quotes/:id', async (req, res, next)=>{
-    try{
+app.put('/quotes/:id', asyncHandler(async (req, res)=>{
         const quote = await records.getQuote(req.params.id);
         if(quote){
             quote.quote = req.body.quote;
@@ -61,23 +57,15 @@ app.put('/quotes/:id', async (req, res, next)=>{
         } else {
             res.status(404).json({message: "Wasn't foud"});
         }
-    }catch (e) {
-        next(err);
-    }
-});
+}));
 
 
 //    - Send a DELETE request to /quotes/:id to DELETE a quote
-app.delete("/quotes/:id", async(req, res, next) =>{
-    try{
-        throw new Error("smth wrong");
+app.delete("/quotes/:id", asyncHandler(async(req, res) => {
         const quote = await records.getQuote(req.params.id);
         await records.deleteQuote(quote);
         res.status(204).end();
-    }catch (err) {
-        next(err);
-    }
-});
+}));
 
 //    - Send a GET request to /quotes/quote/random to READ (view) a random quote
 
@@ -87,7 +75,7 @@ app.use((req, res, next) => {
     next(err);
 });
 
-app.use((err, req, res, next) =>{
+app.use((err, req, res) =>{
     res.status(err.status || 500);
     res.json({
         error: {
